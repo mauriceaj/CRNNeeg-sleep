d00475b7bcfcd06a6515f23162b9e0598ba4e0e5
Maurice Abou Jaoude <majaoude@gmail.com>
2020-03-13 00:31:04 -0400

    Update main.py

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ import numpy as np
 from sleepdetector import Sleepdetector
 
 
-
+#Load Data File
 filepath = './data/data.mat'
 mat_file = sio.loadmat(filepath)
 n_examples = np.shape(mat_file['sig1'])[0]
@@ -16,23 +16,21 @@ x[1] = mat_file['sig2']
 x[2] = mat_file['sig3']
 x[3] = mat_file['sig4']
 
-
+#Load CRNNeeg
 weights_cnn_path =  'cnn_weights.hdf5'
 weights_lstm_path = 'lstm_weights.h5'
 CRNNeeg = Sleepdetector(cnn_path = weights_cnn_path, lstm_path = weights_lstm_path)
 
+#Predict and Plot Sleep Stages
 y_hat = CRNNeeg.predict(x)
-
-
-last_idx = np.shape(y_hat)[0]
 plt.plot(y_hat)
 
 #Compare with original labels, compute Cohen's Kappa and Accuracy
 labels_file = './data/labels.mat'
 y_true = sio.loadmat(labels_file)['labels'].T
+last_idx = np.shape(y_hat)[0]
 n_max = np.minimum(last_idx, np.shape(y_true)[0])
 y_true = y_true[:n_max] - 1
-
 
 ck = cohen_kappa_score(y_true, y_hat)
 accuracy = accuracy_score(y_true, y_hat)
